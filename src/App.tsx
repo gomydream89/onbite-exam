bed8860dc8d4c11793b767c6c12fd649da9d2d99
woori <gomydream89@gmail.com>
2026-04-24 16:49:26 +0900

    Section2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,49 @@
 import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { cn } from "./lib/utils";
+
 import "./App.css";
+import { Toaster } from "./components/ui/sonner";
+import { toast } from "sonner";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 function App() {
-  return <div>Hello world</div>;
+  const isActive = true;
+
+  return (
+    <div className="p-5">
+      <Toaster />
+
+      <Carousel className="mx-10">
+        <CarouselContent>
+          <CarouselItem className="basis-1/3">1</CarouselItem>
+          <CarouselItem>2</CarouselItem>
+          <CarouselItem>3</CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+
+      <Button
+        onClick={() => {
+          toast("토스트메시지", {
+            position: "top-center",
+          });
+        }}
+      >
+        버튼!
+      </Button>
+      <div className={cn(isActive ? "text-green-500" : "text-red-500")}>
+        isActive
+      </div>
+    </div>
+  );
 }
 
 export default App;
